d55f02f5630ac5029489b7282ddc5b7cfb93ff89
Abhishek98451 <90643232+Abhishek98451@users.noreply.github.com>
2022-01-07 23:17:51 +0530

    done
    done

--- mySeedJobs.groovy
+++ mySeedJobs.groovy
@@ -10,22 +10,21 @@ job('myProjectSeed/myProject'){
         git('https://github.com/Abhishek98451/myNodeJsApp.git')
     }
     steps {
-        script {
-          sh "aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com"
-        }
+        
+          shell( "aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com")
+       
           
     }
     // Building Docker images
     steps{
-      script {
-        dockerImage = docker.build "${IMAGE_REPO_NAME}:${IMAGE_TAG}"
-      }
+      shell(dockerImage = docker.build "${IMAGE_REPO_NAME}:${IMAGE_TAG}")
+      
     }
     // Uploading Docker images into AWS ECR
-    steps{  
-        script {
-              sh "docker tag ${IMAGE_REPO_NAME}:${IMAGE_TAG} ${REPOSITORY_URI}:$IMAGE_TAG"
-              sh "docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO_NAME}:${IMAGE_TAG}"
-        }
+    steps{ 
+      shell("docker tag ${IMAGE_REPO_NAME}:${IMAGE_TAG} ${REPOSITORY_URI}:$IMAGE_TAG") 
       }
+    steps{
+      shell("docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO_NAME}:${IMAGE_TAG}")
+    }
 }
