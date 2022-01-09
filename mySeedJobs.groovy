f364d4d49177ba2bcac35870d5a4c15169e14c82
Abhishek98451 <90643232+Abhishek98451@users.noreply.github.com>
2022-01-09 20:28:48 +0530

    Update mySeedJobs.groovy

--- mySeedJobs.groovy
+++ mySeedJobs.groovy
@@ -18,7 +18,7 @@ job('myProjectSeed/myProject'){
     }
     // Building Docker images
     steps{
-      shell(dockerImage ="docker build -t ${IMAGE_REPO_NAME}:${IMAGE_TAG}")
+      shell('dockerImage ="docker build -t ${IMAGE_REPO_NAME}:${IMAGE_TAG}"')
       
     }
     // Uploading Docker images into AWS ECR
